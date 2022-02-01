80141a775bdb52c165c69a29ed705d158ee52a93
Teodora Stevanovic <teodorastevanovic26@gmail.com>
2022-02-01 04:11:59 +0100

    povezan user sa modelom dog

--- laravel-domaci/app/Models/User.php
+++ laravel-domaci/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function dogs()
+    {
+        //jedan korisnik moze da ima vise pasa
+        return $this->hasMany(Dog::class);
+    }
 }
